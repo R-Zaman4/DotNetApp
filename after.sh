70dfdb28d24b9c9f82e1f4afad30f2bf00f751f6
R-Zaman4 <107214835+R-Zaman4@users.noreply.github.com>
2022-06-30 15:47:23 +0530

    Update after.sh

--- after.sh
+++ after.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm nginx -f
-docker run --name nginx  -d -p 80:80 -it 181023512590.dkr.ecr.us-east-1.amazonaws.com/helloworld
+docker run --name nginx  -d -p 80:80 -it 181023512590.dkr.ecr.us-east-1.amazonaws.com/dotnetapp
 echo "Pipeline constructed well"
