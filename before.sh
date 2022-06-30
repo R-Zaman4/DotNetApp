3f2b8015ae39dc16a9145c79e765c20f7f016e78
R-Zaman4 <107214835+R-Zaman4@users.noreply.github.com>
2022-06-30 15:47:55 +0530

    Update before.sh

--- before.sh
+++ before.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 181023512590.dkr.ecr.us-east-1.amazonaws.com
-docker pull 181023512590.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest
+docker pull 181023512590.dkr.ecr.us-east-1.amazonaws.com/dotnetapp:latest
